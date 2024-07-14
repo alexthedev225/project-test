21f338e140a680cfe10854285b4749061aa67d0d
alexthedev225 <alexcode225@gmail.com>
2024-07-14 02:32:49 +0200

    test

--- app/api/recipes/[slug]/route.ts
+++ app/api/recipes/[slug]/route.ts
@@ -1,7 +1,58 @@
+// app/api/recipes/[id]/route.js
+
 export async function GET(
     request: Request,
     { params }: { params: { slug: string } }
   ) {
-    const slug = params.slug // 'a', 'b', or 'c'
-    return Response.json({ slug })
+    const { slug } = params;
+    console.log("Received ID:", slug); // Log pour vérifier l'ID reçu
+  
+    // Données factices
+    const fakeData = [
+      {
+        id: '1',
+        title: 'Recette 1',
+        excerpt: 'Un aperçu de la recette 1',
+        category: 'Dessert',
+        content: 'Voici le contenu de la recette 1',
+        diet: 'Végétarien',
+        calories: 250,
+        imagePath: '/path/to/image1.jpg',
+        ingredients: JSON.stringify(['Ingrédient 1', 'Ingrédient 2']),
+        instructions: JSON.stringify(['Étape 1', 'Étape 2']),
+      },
+      {
+        id: '2',
+        title: 'Recette 2',
+        excerpt: 'Un aperçu de la recette 2',
+        category: 'Entrée',
+        content: 'Voici le contenu de la recette 2',
+        diet: 'Végétalien',
+        calories: 150,
+        imagePath: '/path/to/image2.jpg',
+        ingredients: JSON.stringify(['Ingrédient A', 'Ingrédient B']),
+        instructions: JSON.stringify(['Étape A', 'Étape B']),
+      },
+      {
+        id: '3',
+        title: 'Recette 3',
+        excerpt: 'Un aperçu de la recette 3',
+        category: 'Plat principal',
+        content: 'Voici le contenu de la recette 3',
+        diet: 'Sans gluten',
+        calories: 350,
+        imagePath: '/path/to/image3.jpg',
+        ingredients: JSON.stringify(['Ingrédient X', 'Ingrédient Y']),
+        instructions: JSON.stringify(['Étape X', 'Étape Y']),
+      },
+    ];
+  
+    const recipe = fakeData.find((item) => item.id === slug);
+  
+    if (!recipe) {
+      return new Response(JSON.stringify({ status: 'fail', message: 'Recette non trouvée' }), { status: 404 });
+    }
+  
+    return new Response(JSON.stringify(recipe), { status: 200 });
   }
+  
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,95 @@
-export default function Page({ params }: { params: { slug: string } }) {
-    return <div>My Post: {params.slug}</div>
+// app/recipes/[slug]/page.tsx
+
+import React from 'react';
+import Image from 'next/image';
+
+interface Post {
+  title: string;
+  excerpt: string;
+  category: string;
+  content: string;
+  diet?: string;
+  calories?: number;
+  imagePath: string;
+  ingredients: string;
+  instructions: string;
+}
+
+interface BlogDetailProps {
+  params: {
+    slug: string;
+  };
+}
+
+const fetchPost = async (slug: string): Promise<Post> => {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${baseUrl}/api/recipes/${slug}`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch data: ${res.statusText}`);
   }
+  return res.json();
+};
+
+const BlogDetail: React.FC<BlogDetailProps> = async ({ params }) => {
+  const { slug } = params;
+  const post = await fetchPost(slug);
+  const ingredients = JSON.parse(post.ingredients);
+  const instructions = JSON.parse(post.instructions);
+
+  return (
+    <section className="py-12 bg-gray-50">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-center mb-8 playfair-display text-black">{post.title}</h1>
+        <div className="max-w-4xl mx-auto">
+          <div className="w-full bg-gray-300 mb-8 relative rounded-lg overflow-hidden h-96 text-black">
+            <Image
+              src={post.imagePath}
+              alt={post.title}
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              style={{ objectFit: 'cover' }}
+              className="transition-opacity duration-300 transform hover:opacity-90"
+              priority
+            />
+          </div>
+          <div className="bg-white p-6 shadow-lg rounded-lg text-black">
+            <p className="text-gray-700 text-lg">{post.content}</p>
+
+            {/* Affichage des ingrédients */}
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold mb-4">Ingrédients :</h2>
+              <ul className="list-disc list-inside">
+                {ingredients.map((ingredient: string, index: number) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Affichage des instructions */}
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold mb-4">Instructions :</h2>
+              <ol className="list-decimal list-inside space-y-3">
+                {instructions.map((instruction: string, index: number) => (
+                  <li key={index}>{instruction}</li>
+                ))}
+              </ol>
+            </div>
+
+            {/* Affichage des informations nutritionnelles */}
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold mb-4">Informations nutritionnelles :</h2>
+              <p>
+                <span className="font-semibold">Calories :</span> {post.calories ? `${post.calories} kcal` : 'Non spécifié'}
+              </p>
+              <p>
+                <span className="font-semibold">Régime alimentaire :</span> {post.diet ? post.diet : 'Non spécifié'}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogDetail;
